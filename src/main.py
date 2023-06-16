3f3458d30a19c85b0355e2791b1fbb90ea96c173
Linus Romland <hello@linusromland.com>
2023-06-16 19:07:18 +0200

    Implemented basic ping pong bot

--- src/main.py
+++ src/main.py
@@ -1,27 +1,33 @@
 import os
-from discord import Intents
-from discord.ext import commands
+from discord import Client, Intents, Interaction, Message, Object, app_commands
 
 # Read your API keys from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_ID = os.getenv('GUILD_ID')
 
 # Initialize your bot with the needed intents
 intents = Intents.default()
-intents.typing = False
-intents.presences = False
+intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 
-@bot.event
+@client.event
 async def on_ready():
+    await tree.sync(guild=Object(GUILD_ID if GUILD_ID else ""))
     print(
-        f"Logged in as {bot.user.name if bot.user else 'N/A'} (ID: {bot.user.id if bot.user else 'N/A'})")
+        f"Logged in as {client.user.name if client.user else 'N/A'} (ID: {client.user.id if client.user else 'N/A'})")
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
+@client.event
+async def on_message(message: Message):
+    print(f"{message.author}: {message.content}")
+
+
+@tree.command(name="ping", description="Ping the bot", guild=Object(GUILD_ID) if GUILD_ID else None)
+async def ping(interaction: Interaction):
+    await interaction.response.send_message("Pong!")
 
 
 def main():
@@ -29,7 +35,7 @@ def main():
         print('Please set the DISCORD_TOKEN environment variable.')
         return
 
-    bot.run(DISCORD_TOKEN)
+    client.run(DISCORD_TOKEN)
 
 
 if __name__ == "__main__":
