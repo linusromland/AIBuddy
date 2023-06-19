1697cdc1ef9d9d145349b5c5df1e8b83ad21a7c3
Linus Romland <hello@linusromland.com>
2023-06-19 18:07:14 +0200

    Changed openai model

--- src/utils/openai_api.py
+++ src/utils/openai_api.py
@@ -10,7 +10,7 @@ def generate_response(prompt: List[Dict[str, str]]) -> str:
     """ Generate text using the OpenAI API. """
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-0613",
         messages=prompt,
     )
 
