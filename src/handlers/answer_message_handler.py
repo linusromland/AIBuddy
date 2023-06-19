05dabd8e7f79b343a696b3605ffbc2800b8bbc68
Linus Romland <hello@linusromland.com>
2023-06-19 18:11:38 +0200

    Added !hide and !forget commands

--- src/handlers/answer_message_handler.py
+++ src/handlers/answer_message_handler.py
@@ -3,6 +3,7 @@ from sqlite3 import Connection
 from discord import Client, Message
 from database.queries.general_info import get_information
 from database.queries.misc_info import get_all_misc_info
+from utils.check_permission import check_permission
 from utils.openai_api import generate_response
 
 
@@ -35,8 +36,16 @@ async def answer_message(message: Message, client: Client, conn: Connection):
     ]
 
     async for previous_message in message.channel.history(limit=10):
+        if previous_message.content.startswith("!h"):
+            continue
+
         user = client.get_user(previous_message.author.id)
         bot_message = previous_message.author == client.user
+
+        if (previous_message.content.startswith("!forget")):
+            # Check if user is allowed to forget (admin)
+            if check_permission(conn, message.author.id, False):
+                break
 
         prompt.insert(0, {
             "role": "assistant" if bot_message else "user",
